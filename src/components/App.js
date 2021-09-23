e6cbfb5f993dbaac5f2bc5ddaf0b5d736eabb8e9
Tanidaiz <tanidaig@yahoo.co.jp>
2021-09-23 20:00:25 +0900

    commited

---
+++ src/components/App.js
@@ -0,0 +1,50 @@
+import React,{useState,useEffect} from "react"
+import { messagesRef,pushMessage } from "../firebase"
+
+const App = () => {
+  const [name,setName] = useState('hoge')
+  const [text,setText] = useState('hello')
+  const [messages,setMessages] = useState([{name:"bob",text:"hello"}])
+  useEffect(() => {
+    messagesRef
+      .orderByKey()
+      .limitToLast(10)
+      .on("value", (snapshot) => {
+        const messages = snapshot.val()
+        if (messages === null) return
+        const entries = Object.entries(messages)
+        const newMessages = entries.map((data) => {
+          const [key, message] = data
+          return { key, ...message }
+        })
+        setMessages(newMessages)
+      })
+  }, [])
+
+  return(
+    <>
+      {messages.map((message) => (
+        <div key={message.key}>
+          {message.name}:{message.text}
+        </div>
+      ))}
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName((name) => (name = e.target.value))}
+      />
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText((text) => (text = e.target.value))}
+      />
+      <button onClick={() => pushMessage({ name: name, text: text })}>
+        {" "}
+        push{" "}
+      </button>
+    </>
+  )
+    
+}
+
+export default App
